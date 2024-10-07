93ba37607db69a0c6d287b2cb346b407dc9c2079
qhyabdoel <qhyabdoel@gmail.com>
2024-10-07 22:12:52 +0700

    Initial commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,15 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            User Management
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            
+                <label class="text-gray-500 font-bold text-4xl">This is Dashboard</label>
+            
+        </div>
+    </div>
+</x-app-layout>
